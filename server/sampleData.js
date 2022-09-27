6134b14f71ce7312f5dc07acd6f6ab43b26d5a66
Jonathan Satoru Toyoshima <jonathan.toyoshima@lojasrenner.com.br>
2022-09-26 21:22:52 -0300

    Project & queries

---
+++ server/sampleData.js
@@ -0,0 +1,20 @@
+const projects = [
+  {
+    id: "1",
+    clientID: "1",
+    name: "eCommerce Wb",
+    description: "Hellows",
+    status: "In Progress",
+  },
+];
+
+const clients = [
+  {
+    id: "1",
+    name: "Fulano de tal",
+    email: "email@email.com",
+    phone: "(51) 98765-4321",
+  },
+];
+
+module.exports = { projects, clients };
